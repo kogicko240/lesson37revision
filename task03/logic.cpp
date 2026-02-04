b4ae6a7d15428f432f62c2c9f6e399df19918627
kogicko240 <dmn120112@gmail.com>
2026-02-04 18:36:18 +0300

    Update main.cpp

--- task03/logic.cpp
+++ task03/logic.cpp
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 //#include "logic.h"
 //
 //void printNumbersOptimized(int N, int M) {
-//    cout << "The -resulting numerical sequence: ";
-//
+//    cout << "The resulting numerical sequence: ";
+//    
 //    int step = (N < M) ? 1 : -1;
 //
 //    int end = M + step;
@@ -16,22 +15,3 @@
 //    }
 //    cout << endl;
 //}
-=======
-#include "logic.h"
-
-void printNumbersOptimized(int N, int M) {
-    cout << "The resulting numerical sequence: ";
-    
-    int step = (N < M) ? 1 : -1;
-
-    int end = M + step;
-
-    for (int i = N; i != end; i += step) {
-        cout << i;
-        if (i + step != end) {
-            cout << " ";
-        }
-    }
-    cout << endl;
-}
->>>>>>> b32e5a29173520eb53006e0e9301ec7ff4ce8166
